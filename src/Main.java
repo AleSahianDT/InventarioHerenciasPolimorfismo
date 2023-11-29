d1921616f0affc1508dd218bd9d3f06f7b31d8d8
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2023-11-29 07:41:11 -0500

    A

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        //Producto p1 = new Producto("Producto genérico 1",0,"C001",0,"Sin marca");
-        Producto p1 = new Producto();
-        p1.ingresarProducto();
-        Alimento a1 = new Alimento();
-        a1.ingresarProducto();
 
-        Electrodomestico e1 = new Electrodomestico();
-        e1.ingresarProducto();
+        List<Producto> listaProductos = new ArrayList<>();
+
+        Alimento alimento = new Alimento("alimento","Leche", 1.10, "P0001", 250, "Vita",
+                "27/11/2023", "26/12/2023", null);
+        Tecnologia tecnologia = new Tecnologia("tecnologia","Laptop", 600, "P0002", 100, "HP", "HP", 110);
+        Electrodomestico electrodomestico = new Electrodomestico("electronomestico","Cocina", 250, "P0003",
+                50, "Olimpia", "Olimpia", 220, 5);
+
+        listaProductos.add(alimento);
+        listaProductos.add(tecnologia);
+        listaProductos.add(electrodomestico);
+
+        Alimento alimento2 = (Alimento) listaProductos.get(0);
+
+        for (Producto producto: listaProductos) {
+            if(producto.tipo.equals("alimento")){
+                Alimento alimento = (Alimento)producto;
+                System.out.println("El alimento "+alimento.nombre+"");
+            }
+        }
+
+
     }
 }
